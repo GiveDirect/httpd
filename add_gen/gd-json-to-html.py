d19dbe07355c29cb23c1fa03a089db8cb11529d3
joezippy <kcolussi@gmail.com>
2018-05-05 13:52:06 -0600

    Added Ripple (XRP) address output
    Had to add special markup to format the required ripple destination tag properly...

--- add_gen/gd-json-to-html.py
+++ add_gen/gd-json-to-html.py
@@ -2,11 +2,15 @@
 import glob
 import json
 import datetime
+import re
 
 #########
 # NOTE: It's recommended by Fritz that this script run twice a day to keep addresses fresh in case they move there servers.
 #########
 
+ripple_re = "\*{2}__Deposit"
+ripple_addr_re = "r[0-9a-zA-Z]{33}"
+ripple_dest_re = "[0-9]{5}.*[0-9]"
 QR_GEN_URL = "https://chart.googleapis.com/chart?cht=qr&chl=%s&chs=160x160&chld=L|0"
 XLM_EXPLORER_URL = "https://stellar.expert/explorer/public/account/"
 
@@ -43,9 +47,20 @@ if __name__ == "__main__":
                         print(qr_address)
                         out = out + "<br>XLM: " + xlm_address + "(<a href=" + qr_address + ">QR</a>)(<a href=" + XLM_EXPLORER_URL + xlm_address + ">Details</a>)"
                         first_time = False
-                    qr_address = QR_GEN_URL % (asset_code_address)
-                    out = out + "<br>" + asset_code + ": " + asset_code_address + "(<a href=" + qr_address + ">QR</a>)"
-                    print(paywall + " - " + asset_code + " - " + xlm_address + " - " + asset_code_address)
+
+                    match = re.search(ripple_re, asset_code_address)
+                    if (match):
+                        match = re.search(ripple_addr_re, asset_code_address)
+                        ripple_addr = match.group(0)
+
+                        match = re.search(ripple_dest_re, asset_code_address)
+                        ripple_dest = match.group(0)
+                        out = out + "<br>" + asset_code + ": " + ripple_addr + " (<font color=\"FF0000\">Required Dest. Tag: "  + ripple_dest + "</font>)"
+                        print(paywall + " - " + asset_code + " - " + xlm_address + " - " + ripple_addr + " - " + ripple_dest)
+                    else:
+                        qr_address = QR_GEN_URL % (asset_code_address)
+                        out = out + "<br>" + asset_code + ": " + asset_code_address + "(<a href=" + qr_address + ">QR</a>)"
+                        print(paywall + " - " + asset_code + " - " + xlm_address + " - " + asset_code_address)
             out = out + "</pre></body></html>"
             with open(output_file, 'w+') as data_file:
                 data_file.write(out)
